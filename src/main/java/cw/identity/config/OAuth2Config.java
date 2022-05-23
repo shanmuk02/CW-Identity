efc69875442a981ec156b17da126f316e6a8bd9b
Pranav Sugandhi <pranav.sugandhi@exceloid.com>
2022-05-23 13:31:36 +0530

    Added log for identityServer

--- src/main/java/cw/identity/config/OAuth2Config.java
+++ src/main/java/cw/identity/config/OAuth2Config.java
@@ -20,9 +20,11 @@ import cw.identity.redis.config.CustomRedisTokenStore;
 
 @Configuration
 public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
-	
-	@Value("#{new Integer('${token.timeout}')}") private int tokenTimeout;
-	@Value("#{('${tokenstore.type}')}") private String tokenStoreType;
+
+	@Value("#{new Integer('${token.timeout}')}")
+	private int tokenTimeout;
+	@Value("#{('${tokenstore.type}')}")
+	private String tokenStoreType;
 	private String clientId = "talk2amareswaran";
 	private String clientSecret = "my-secret";
 	private String privateKey = "123";
@@ -30,45 +32,45 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 	@Autowired
 	@Qualifier("authenticationManagerBean")
 	private AuthenticationManager authenticationManager;
-	
+
 	@Autowired
 	private MD5PasswordEncoder passwordEncoder;
-	
+
 	@Bean
 	public JwtAccessTokenConverter tokenEnhancer() {
 		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
 		converter.setSigningKey(privateKey);
 		return converter;
 	}
-	
+
 	public TokenStore tokenStore() {
-		if(tokenStoreType.equalsIgnoreCase("MONGO"))
+		if (tokenStoreType.equalsIgnoreCase("MONGO"))
 			return new MongoTokenStore();
-		else if(tokenStoreType.equalsIgnoreCase("REDIS"))
+		else if (tokenStoreType.equalsIgnoreCase("REDIS"))
 			return new CustomRedisTokenStore(new JedisConnectionFactory());
 		else
 			return new JwtTokenStore(tokenEnhancer());
-    }
-	
+	}
+
 //	public JwtTokenStore tokenStore() {
 //		return new JwtTokenStore(tokenEnhancer());
 //	}
-	
+
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore())
 				.accessTokenConverter(tokenEnhancer());
 	}
-	
+
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 		security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
 	}
-	
+
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient(clientId).secret(passwordEncoder.encode(clientSecret))
-			.scopes("read", "write").authorizedGrantTypes("password", "refresh_token")
-			.accessTokenValiditySeconds(tokenTimeout*60).refreshTokenValiditySeconds(tokenTimeout*60);
+		clients.inMemory().withClient(clientId).secret(passwordEncoder.encode(clientSecret)).scopes("read", "write")
+				.authorizedGrantTypes("password", "refresh_token").accessTokenValiditySeconds(tokenTimeout * 60)
+				.refreshTokenValiditySeconds(tokenTimeout * 60);
 	}
 }
